e2110af8f2bc3a80c8a3e7468722f50979ec6a12
yhwit30 <yhwit30@gmail.com>
2023-12-29 14:30:07 +0900

    article detail 1만 구현. charAt이라 불안정함

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		List<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
+		// 날짜 변수
 
 		while (true) {
 			System.out.print("입력된 명령어>");
@@ -25,6 +26,7 @@ public class Main {
 
 			if (cmd.equals("article write")) {
 				int id = lastArticleId + 1;
+				// 날짜 변수도 객체에 넣고
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
@@ -34,6 +36,7 @@ public class Main {
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 				lastArticleId++;
+
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시글 목록 ==");
 				if (articles.size() == 0) {
@@ -47,7 +50,35 @@ public class Main {
 					System.out.printf("%4d  /  %s\n", article.getId(), article.getTitle());
 				}
 
-			} else {
+			} else if (cmd.startsWith("article detail")) {
+				if (cmd.charAt(15) == '1') {
+					if (articles.size() == 0) {
+						System.out.printf("%d번 게시글은 없습니다.\n", 1);
+					} else if(articles.size() > 0) {
+						Article article = articles.get(0);
+						System.out.println("번호 : " + article.getId());
+						System.out.println("날짜 : 2023-12-12 12:12:12");
+						System.out.println("제목 : " + article.getTitle());
+						System.out.println("내용 : " + article.getbody());
+					}
+					
+
+				}
+				if (cmd.charAt(15) == '2') {
+					if (articles.size() == 1) {
+						System.out.printf("%d번 게시글은 없습니다.\n", 2);
+					}else if(articles.size() > 0) {
+						Article article = articles.get(1);
+						System.out.println("번호 : " + article.getId());
+						System.out.println("날짜 : ");
+						System.out.println("제목 : " + article.getTitle());
+						System.out.println("내용 : " + article.getbody());
+					
+					}
+				}
+			}
+
+			else {
 				System.out.println("사용할 수 없는 명령어입니다.");
 			}
 
